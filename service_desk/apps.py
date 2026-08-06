481443a893736da5078e04bf99fa48ba931a6d12
Bryan Cordero <bryanzeta17@gmail.com>
2026-08-06 16:29:57 -0500

    feat: add service_desk app with initial configuration and files

--- service_desk/apps.py
+++ service_desk/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ServicedeskConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'servicedesk'
+    name = 'service_desk'
